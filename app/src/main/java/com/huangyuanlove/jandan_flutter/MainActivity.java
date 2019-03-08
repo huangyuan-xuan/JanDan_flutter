c9d25eb29799388abfef1ec88978cf5d4dd3796b
huangyuan <huangyuan@chunyu.me>
2019-03-08 11:22:56 +0800

    混编

---
+++ app/src/main/java/com/huangyuanlove/jandan_flutter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.huangyuanlove.jandan_flutter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.flutter.facade.Flutter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView( Flutter.createView(this,getLifecycle(),"main"));
+
+    }
+}
