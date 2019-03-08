dc2d7c9c3f77f3f4c286a69bfcd8f7d7481ee962
huangyuan <huangyuan@chunyu.me>
2019-03-08 11:32:52 +0800

    替换为Android X

--- app/src/main/java/com/huangyuanlove/jandan_flutter/MainActivity.java
+++ app/src/main/java/com/huangyuanlove/jandan_flutter/MainActivity.java
@@ -1,6 +1,6 @@
 package com.huangyuanlove.jandan_flutter;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import io.flutter.facade.Flutter;
